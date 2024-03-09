f164304258187c44c490f46e07e6d0aeba5e41a0
n00bcybot <freshmaker21@gmail.com>
2024-03-09 14:23:54 +0100

    Cleaner log

--- Sync-Folder.ps1
+++ Sync-Folder.ps1
@@ -1,13 +1,19 @@
 
-# DRAFT
-
 $source_folder = "C:\Users\fresh\Desktop\Veeam\source"
 $backup_folder = "C:\Users\fresh\Desktop\Veeam\backup"
 $log_folder = "C:\Users\fresh\Desktop\Veeam\logs\activity.log"
-
 $source_list = Get-ChildItem -Path $source_folder
 
-# Check if each file in the source folder exists in the backup folder already. Using parallel for faster processing
+$date = Get-Date
+$output = "###   $date" +  " ###  New log started at $log_folder  ###" +
+"`n###--------------------------------------------------------------------------------------------------------###`n" 
+
+Write-Host $output
+$output | Out-File -Append -FilePath $log_folder | Wait-Process
+
+
+# Check if each file in the source folder exists in the backup folder already. Using "parallel" switch for faster processing.
+# ForEach-Object -Parallel runs in a separate runspace, so it doesn't see the variables in the body
 $source_list | ForEach-Object -Parallel {
     
     $date = Get-Date
@@ -18,21 +24,32 @@ $source_list | ForEach-Object -Parallel {
     # Check if the file exists in the backup folder. If it doesn't, copy it from the source folder
     if (!(Test-Path -Path $($folder + "\" + $_.Name))){
         
-        Write-Output $_.Name " not found in $folder, copying..."
+        $output = "$date " + $_.Name + " not found in $folder, copying..."
+        # Log to console
+        Write-Host $output
+        # Log to file
+        $output | Out-File -Append -FilePath $log_path | Wait-Process
         Copy-Item -Path $path -Destination $folder
     
     # If it does exist, check if the source file has been modified by comparing the last time both files have been modified. If the source
-    # file is newer, rewrite the backup file with it, otherwise do nothing. That way, only those files which have been modified will be 
-    # replaced, instead of all files. This should save a lot of time, if the folder contains large and numerous files. 
+    # file is newer, overwrite the backup file with it, otherwise do nothing. That way, only those files which have been modified will be 
+    # replaced, instead of all files. This should save a lot of time, if the folder contains large files and/or large number of them. 
     }elseif (Test-Path -Path $($folder + "\" + $_.Name)) {
         if ($_.LastWriteTime -gt (Get-ChildItem -Path $($folder + "\" + $_.Name)).LastWriteTime){
-            $output = "$date " + $_.Name + " found in $folder, rewriting..." 
+            
+            $output = "$date " + $_.Name + " found in $folder, overwriting..." 
             # Log to console
-            Write-Host $output
+            Write-Host $output 
             # Log to file
-            $output | Out-File -Append -FilePath $log_path
+            $output | Out-File -Append -FilePath $log_path | Wait-Process
             Copy-Item -Path $path -Destination $folder
         }
     } 
 }
-    
+
+$date = Get-Date
+$output = "`n###--------------------------------------------------------------------------------------------------------###" +
+"`n###   $date" +  " ###  Log ended  ###" 
+
+Write-Host $output 
+$output | Out-File -Append -FilePath $log_folder
