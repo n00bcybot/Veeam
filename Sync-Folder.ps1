3c76299e66b1b25db9cb3f8a8d4ec62f8c58e6b3
n00bcybot <freshmaker21@gmail.com>
2024-03-12 23:27:47 +0100

    Minor adjustments

--- Sync-Folder.ps1
+++ Sync-Folder.ps1
@@ -1,34 +1,40 @@
 <#
 .SYNOPSIS
-    Syncronises the content of a given folder to another folder
+        Syncronises the content of a given folder to another folder
 
 .DESCRIPTION
-    This function syncronyzes the content of a given folder to another folder. It has five paramters, three of which are mandatory, for the
+        This function syncronyzes the content of a given folder to another folder. It has five paramters, three of which are mandatory, for the
     folders paths, one optional parameter to define at what interval the function should run in seconds (the default is 10 seconds), and one 
     optional parameter with which a time argument can be provided (hour and minute in the format "HH:mm") if the function should stop running
     at specific time in the same day. 
-    The function starts by getting a list of all files in the source folder and checks if the same file exists in the backup folder. 
+        The function starts by getting a list of all files in the source folder and checks if the same file exists in the backup folder. 
     If it doesn't, it copies it and if it does exist, checks if the file has been modified at earler date than the corresponding file in the 
     source folder and if so, overwrites it. All operations are output to console and a log file with a time stamp, as well as the file names 
     and folder paths.
      
 .NOTES
-    - for itteration over the files in the source folder, the function uses parallel processing with "ForEach-Object -Parallel", since this is a
-        simple way to speed up this process. For a folder containing large number of files, multiprocessing solution with PowerShell jobs might be 
-        more suitable. "ForEach-Object -Parallel" executes in a different runspace than the current one, therefore "using" is used to import the 
-        variables. 
-    - the functions assumes that the back folder has already been created and does not check if it contains files that are not found
-        in the source folder.
-    - Wait-Process ensures that only one process at a time has been recorded in the log and  
-    - Certainly different conditional logic can be used in the "until" section of the do-until loop, depending on specific use
+        For itteration over the files in the source folder, the function uses parallel processing with "ForEach-Object -Parallel", since this is a
+    simple way to speed up this process. For a folder containing large number of files, multiprocessing solution with PowerShell jobs might be 
+    more suitable. "ForEach-Object -Parallel" executes in a different runspace than the current one, therefore "using" is used to import the 
+    variables. 
+        The functions assumes that the back folder has already been created and does not check if it contains files that are not found
+    in the source folder.
+        Wait-Process ensures that only one process at a time has been recorded in the log and  
+        Certainly different conditional logic can be used in the "until" section of the do-until loop, depending on specific use, or different
+    loop could be used altogether.
+        If more convinient, the script can be stripped from the function, while keeping the parameters, so that the ps1 file itself can be run
+    from the command line, instead of the code, and still accept arguments.
+        More fail save blocks can be added, as well as console inputs for more granular control, depending on the usecase.
 
 .EXAMPLE
-    Log-Output -SourceFolder "C:\source" -BackupFolder "C:\backup" -LogFolder "C:\Logs"
-    Log-Output -SourceFolder "C:\source" -BackupFolder "C:\backup" -LogFolder "C:\Logs" -Interval 230 -EndTime "15:44"
+
+    Example 1: Sync-Folder -SourceFolder "C:\source" -BackupFolder "C:\backup" -LogFolder "C:\Logs"
+
+    Example 2: Sync-Folder -SourceFolder "C:\source" -BackupFolder "C:\backup" -LogFolder "C:\Logs" -Interval 230 -EndTime "15:44"
 #>
 
 
-function Log-Output {
+function Sync-Folder {
     [CmdletBinding()]
     param(
         [Parameter(Mandatory=$true)][string]$SourceFolder,
@@ -39,16 +45,16 @@ function Log-Output {
         [switch]$Verify
     )
 
+    # Check if the paths exist and indeed point to a folder
     $paths = $SourceFolder, $BackupFolder, $LogFolder
     foreach ($item in $paths) {
         if ( !(Test-Path -Path $item) -or !(Test-Path -Path $item -PathType Container) ){
-            Write-Host "Folder $item doesn't exsist or provied path is not a folder"
+            Write-Output "Folder $item doesn't exsist or provied path is not a folder"
             break
         }else {
             
-            $LogPath = $LogFolder + "\" + "activities.log"
-            
-
+            $LogPath = "$LogFolder" + "\" + "activities.log"
+                        
             do {
                 $source_list = Get-ChildItem -Path $SourceFolder
                 $date = Get-Date
@@ -56,9 +62,8 @@ function Log-Output {
                 $output = "###   $date" +  " ###  New log started at $LogPath  ###" +
                 "`n###--------------------------------------------------------------------------------------------------------###`n" 
 
-                Write-Host $output
+                Write-Output $output
                 $output | Out-File -Append -FilePath $LogPath | Wait-Process 
-
 
                 # Check if each file in the source folder exists in the backup folder already. Using "parallel" switch for faster processing.
                 # ForEach-Object -Parallel runs in a separate runspace, so it doesn't see the variables in the body
@@ -74,7 +79,7 @@ function Log-Output {
                         
                         $output = "$date " + $_.Name + " missing in $backup, copying from $using:SourceFolder"
                         # Log to console
-                        Write-Host $output
+                        Write-Output $output
                         # Log to file
                         $output | Out-File -Append -FilePath $log | Wait-Process
                         Copy-Item -Path $source -Destination $backup 
@@ -87,7 +92,7 @@ function Log-Output {
                             
                             $output = "$date " + $_.Name + " already present in $backup, overwriting..." 
                             # Log to console
-                            Write-Host $output 
+                            Write-Output $output 
                             # Log to file
                             $output | Out-File -Append -FilePath $log | Wait-Process
                             Copy-Item -Path $source -Destination $backup 
@@ -95,48 +100,56 @@ function Log-Output {
                     } 
                 }
 
-
                 Start-Sleep $Interval
-
                 
                 if($Verify){
                     
                     $source_list = Get-ChildItem -Path $SourceFolder
                     $result = $source_list | ForEach-Object -Parallel {
                         
-                        $LogPath = $LogFolder + "\" + "activities.log"
-                        if ((Test-Path -Path $_.FullName) -and (Test-Path -Path $($using:BackupFolder + "\" + $_.Name))){
-                            if (((Get-ChildItem -Path $_).LastWriteTime) -eq ((Get-ChildItem -Path $($using:BackupFolder + "\" + $_.Name)).LastWriteTime)){
+                        $LogPath = $using:LogFolder + "\" + "activities.log"
+                        $TestPath = $($using:BackupFolder + "\" + $_.Name)
+
+                        if ((Test-Path -Path $_.FullName) -and (Test-Path -Path $TestPath)){
+                            if (((Get-ChildItem -Path $_).LastWriteTime) -eq ((Get-ChildItem -Path $TestPath).LastWriteTime)){
                                 continue
+                                Write-Output $TestPath
                             }else{
                                 $output = "$_ present, but last write time doesn't match"
-                                Write-Host $output 
+                                Write-Output $output 
                                 $output | Out-File -Append -FilePath $LogPath | Wait-Process
                             }
                                 continue
                             }else{
                             $output = "$_ not found in the backup folder"
-                            Write-Host $output 
+                            Write-Output $output
                             $output | Out-File -Append -FilePath $LogPath | Wait-Process
                         }
                     }
+
                     
-                    if ($result){
-                        Write-Host $result 
+                    if ($result){                        
                         $result | Out-File -Append -FilePath $LogPath | Wait-Process
+
                     }else{
                         $date = Get-Date
                         $output = "`nBackup content successfully verified at " + "$date`n"
-                        Write-Host $output
+                        Write-Host $output -ForegroundColor Green
                         $output | Out-File -Append -FilePath $LogPath | Wait-Process
                         
                     }
+
+                    foreach ($item in (Get-ChildItem -Path $BackupFolder).Name){
+                        if (!($source_list.Name.Contains($item))){
+                            Write-Warning "$item not found in source folder!"
+                        }
+                    }                    
                 }
 
                 $output = "`n###--------------------------------------------------------------------------------------------------------###" +
                 "`n###   $date" +  " ###  Log ended  ###`n" 
 
-                Write-Host $output 
+                Write-Output $output 
                 $output | Out-File -Append -FilePath $LogPath
  
             }until ($(Get-Date -Format "HH:mm") -ge $EndTime)
